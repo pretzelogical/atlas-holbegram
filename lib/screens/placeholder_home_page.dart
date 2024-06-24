56b28b5fbde50500f798392a0d40a1d18573b29c
pretzelogical <yingyangyo266@gmail.com>
2024-06-24 12:19:21 -0500

    Add placeholder home page

---
+++ lib/screens/placeholder_home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:holbegram/screens/login_screen.dart';
+import 'package:holbegram/screens/signup_screen.dart';
+import 'package:holbegram/methods/auth_methods.dart';
+
+class PlaceholderHomePage extends StatelessWidget {
+  const PlaceholderHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(
+      padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+      children: [
+        const Center(
+            child: Text('Holbegram placeholder home',
+                style: TextStyle(fontSize: 20))),
+        Center(
+          child: Column(children: [
+            FilledButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SignupScreen(
+                                emailController: TextEditingController(),
+                                usernameController: TextEditingController(),
+                                passwordController: TextEditingController(),
+                                passwordConfirmController:
+                                    TextEditingController(),
+                              )));
+                },
+                child: const Text('Login screen')),
+            FilledButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LoginScreen(
+                                emailController: TextEditingController(),
+                                passwordController: TextEditingController(),
+                                authMethods: AuthMethods(),
+                              )));
+                },
+                child: const Text('Signup screen'))
+          ]),
+        )
+      ],
+    ));
+  }
+}
